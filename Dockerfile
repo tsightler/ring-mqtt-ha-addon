64132595d49cd246217faad231a90777333290f3
tsightler <tsightler@gmail.com>
2020-02-19 20:02:48 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+ARG BUILD_FROM
+FROM $BUILD_FROM
+ENV LANG C.UTF-8
+RUN apk add --no-cache nodejs npm git
+RUN git clone https://github.com/tsightler/ring-mqtt.git
+COPY run.sh /
+RUN chmod +x run.sh 
+CMD [ "/run.sh" ]
---
+++ run.sh
@@ -0,0 +1,12 @@
+echo Ring Device Integration via MQTT
+echo Node Version
+node -v
+echo NPM Version
+npm -v
+git --version
+
+cd ring-mqtt
+npm install
+npm audit fix
+chmod +x ring-mqtt.js
+DEBUG=ring-mqtt HASSASSON=true /ring-mqtt/ring-mqtt.js
