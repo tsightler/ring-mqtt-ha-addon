1c51e8c6492abaf986f65b4d66fc137474c3c0a8
tsightler <tsightler@gmail.com>
2020-02-24 14:41:29 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-echo Ring Device Integration via MQTT
-echo Node Version
-node -v
-echo NPM Version
-npm -v
+echo "-------------------------------------------------------"
+echo "| Ring Device Integration via MQTT                    |"
+echo "| Addon for Hass.io                                   |"
+echo "|                                                     |"
+echo "| Report issues at:                                   |"
+echo "| https://github.com/tsightler/ring-mqtt-hassio-addon |"
+echo "-------------------------------------------------------"
+echo Node version $(node -v)
+echo NPM version $(npm -v)
 git --version
-
+echo
 cd ring-mqtt
-npm install
+if [ ! -d "node_modules" ]; then
+    echo "Installing dependent packages..."
+    npm install
+fi
+echo "Checking for security issues in dependent packages..."
 npm audit fix
 chmod +x ring-mqtt.js
+echo Running ring-mqtt.js version $(cat package.json | grep version | cut -f4 -d'"')...
 DEBUG=ring-mqtt HASSADDON=true /ring-mqtt/ring-mqtt.js
